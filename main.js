0b97a1a436d500d0fd28d0e1fc836494bf777bcf
Caio Lucas <151796198+CaioLiGS@users.noreply.github.com>
2024-10-23 11:53:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function adicionarTarefa(){
+    let li = document.createElement('li')
+    let input = document.formPrincipal.tarefa.value
+    let input_texto = document.createTextNode(input)
+
+    li.appendChild(input_texto)
+
+    let ul = document.querySelector(".tarefas")
+    ul.appendChild(li)
+
+    document.formPrincipal.tarefa.value = ""
+
+    criarBotaoFechar(li, ul)
+}
+
+function criarBotaoFechar(li, ul){
+    let rotulo = document.createElement('SPAN')
+    let fechar = document.createTextNode('X')
+
+    rotulo.className = "close"
+    rotulo.appendChild(fechar)
+
+    ul.appendChild(rotulo)
+
+    rotulo.onclick = () => {
+        li.style.display = "none"
+        rotulo.style.display = "none"
+    }
+}
+
+document.querySelectorAll('LI').forEach(criarBotaoFechar)
+document.querySelector('ul').addEventListener('click', (e) => {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('checked')
+    }
+})
